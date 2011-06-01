fd2e28040fae795a02b2352618a425b3e9a3cd5f
Cherny <cherny.berbesi@gmail.com>
2011-06-01 15:59:53 -0430

    Added auto-sliding feature

--- src/gallery.py
+++ src/gallery.py
@@ -13,60 +13,60 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-      username = 'cherny.berbesi@seiteca.com'
-      password = '22,e6G1x'
-      source = 'AlvaroGreenSite'
-      if self.request.get('album'):
-        if '-' in self.request.get('album'):
-          _category, _album = self.request.get('album').split('-')
+        username = 'cherny.berbesi@seiteca.com'
+        password = 'xxx'
+        source = 'AlvaroGreenSite'
+        if self.request.get('album'):
+            if '-' in self.request.get('album'):
+                _category, _album = self.request.get('album').split('-')
+            else:
+                _category, _album = [self.request.get('album'), None]
         else:
-          _category, _album = [self.request.get('album'), None]
-      else:
-        _category, _album = ['inicio', 'inicio']
-      gd_client = gdata.photos.service.PhotosService()
-      gd_client.email = username
-      gd_client.password = password
-      gd_client.source = source
-      try:
-        gd_client.ProgrammaticLogin()
-      except BadAuthentication:
-        self.response.out.write('Problema de autenticaci&oacute;n')
-        mail.send_mail(sender="cherny.berbesi@gmail.com>",
-                       to="Cherny D. C. Berbes&iacute; I. <cherny.berbesi@gmail.com>",
-                       subject="Problema con la pagina Web alvarezgreen.com",
-                       body="""Esta recibiendo este correo porque cambio su contrasena de Google y altero el servicio de picasa""")
-        return
-      albums_entry = gd_client.GetUserFeed()
-      photos = list()
-      i = 0
-      albums = list()
-      photo_selected = None
-      for album_entry in albums_entry.entry:
-        if album_entry.title.text != 'inicio':
-          category, album = album_entry.title.text.split('-')
-        else:
-          category, album = ['inicio', 'inicio']
-        if category == _category:
-          albums.append({'category' : category, 'album' : album})
-          if _album is None:
-            _album = album
-          if album_entry.title.text == str(_category + "-" + _album) or _category == 'inicio':
-            photos_entry = gd_client.GetFeed('/data/feed/api/user/%s/albumid/%s?kind=photo' % (username, album_entry.gphoto_id.text))
-            for photo_entry in photos_entry.entry:
-              photo = {
-                'albumId' : photo_entry.albumid.text,
-                'photoId' : photo_entry.gphoto_id.text,
-                'thumbnailUrl' : photo_entry.media.thumbnail[0].url,
-                'url' : photo_entry.content.src
-              }
-              if photo_selected is None:
-                photo_selected = photo
-              photos.append(photo)
-              i += 0
-#        logging.info('title: %s, number of photos: %s, id: %s' % (album_entry.title.text, album_entry.numphotos.text, album_entry.gphoto_id.text))
-      templates_values = { 'album_selected' : _album , 'photos' : photos, 'albums' : albums, 'photo_selected' : photo_selected}
-      path = os.path.join(os.path.dirname(__file__), 'templates/gallery.html')
-      self.response.out.write(template.render(path, templates_values))
+            _category, _album = ['inicio', 'inicio']
+        gd_client = gdata.photos.service.PhotosService()
+        gd_client.email = username
+        gd_client.password = password
+        gd_client.source = source
+        try:
+            gd_client.ProgrammaticLogin()
+        except BadAuthentication:
+            self.response.out.write('Problema de autenticaci&oacute;n')
+            mail.send_mail(sender="cherny.berbesi@gmail.com>",
+                           to="Cherny D. C. Berbes&iacute; I. <cherny.berbesi@gmail.com>",
+                           subject="Problema con la pagina Web alvarezgreen.com",
+                           body="""Esta recibiendo este correo porque cambio su contrasena de Google y altero el servicio de picasa""")
+            return
+        albums_entry = gd_client.GetUserFeed()
+        photos = list()
+        i = 0
+        albums = list()
+        photo_selected = None
+        for album_entry in albums_entry.entry:
+            if album_entry.title.text != 'inicio':
+                category, album = album_entry.title.text.split('-')
+            else:
+                category, album = ['inicio', 'inicio']
+            if category == _category:
+                albums.append({'category' : category, 'album' : album})
+                if _album is None:
+                    _album = album
+                if album_entry.title.text == str(_category + "-" + _album) or _category == 'inicio':
+                    photos_entry = gd_client.GetFeed('/data/feed/api/user/%s/albumid/%s?kind=photo' % (username, album_entry.gphoto_id.text))
+                    for photo_entry in photos_entry.entry:
+                        photo = {
+                            'albumId' : photo_entry.albumid.text,
+                            'photoId' : photo_entry.gphoto_id.text,
+                            'thumbnailUrl' : photo_entry.media.thumbnail[0].url,
+                            'url' : photo_entry.content.src
+                        }
+                        if photo_selected is None:
+                            photo_selected = photo
+                        photos.append(photo)
+                        i += 0
+        #        logging.info('title: %s, number of photos: %s, id: %s' % (album_entry.title.text, album_entry.numphotos.text, album_entry.gphoto_id.text))
+        templates_values = { 'album_selected' : _album , 'photos' : photos, 'albums' : albums, 'photo_selected' : photo_selected}
+        path = os.path.join(os.path.dirname(__file__), 'templates/gallery.html')
+        self.response.out.write(template.render(path, templates_values))
 
 application = webapp.WSGIApplication([('/gallery', MainPage)], debug=True)
 
--- src/js/functions.js
+++ src/js/functions.js
@@ -1,31 +1,38 @@
 var images = {};
 var next_image = undefined;
 
-$(document).ready(function() {
-	set_content_album_links_height();
-	load_images();
-	$(".content_thumbnail img").click(function() {
-		var ii = $(this);
-		if ($(".content_picture img").attr("alt") == ii.attr("alt").substring(0, ii.attr("alt").indexOf("-"))) {
-			return;
-		}
-		next_image = this;
-		load_image();
-	});
-	$(".content_picture").click(load_next_image);
-});
+$(document).ready(
+		function() {
+			set_content_album_links_height();
+			load_images();
+			$(".content_thumbnail img").click(
+					function() {
+						var ii = $(this);
+						if ($(".content_picture img").attr("alt") == ii.attr("alt").substring(0, ii.attr("alt").indexOf("-"))) {
+							return;
+						}
+						next_image = this;
+						load_image();
+					});
+			$(".content_picture").click(load_next_image);
+			setTimeout("start_sliding()", 7000);
+		});
+
+function start_sliding() {
+	$(".content_picture").click();
+	setTimeout("start_sliding()", 7000);
+}
 
 function load_next_image() {
 	var next_position = $("img[alt^='" + $(this).children("img").attr("alt") + "-']").parent().index();
 	if (next_position < 10) {
 		next_image = $(".content_thumbnail").eq(next_position).children("img");
-	}
-	else {
+	} else {
 		next_image = $(".content_thumbnail").eq(0).children("img");
 	}
 	load_image();
-//	$(".content_thumbnail").eq($("img[alt^='5600398738051520754-']").parent().index()+1)
-//	$("img[alt|=" + $(this).attr("alt") + "]");	
+	// $(".content_thumbnail").eq($("img[alt^='5600398738051520754-']").parent().index()+1)
+	// $("img[alt|=" + $(this).attr("alt") + "]");
 }
 
 function load_images() {
@@ -49,7 +56,7 @@ function load_image() {
 		$(".content_picture img").replaceWith(images[$(next_image).attr("alt").split("-")[0]]);
 		$(this).fadeIn(200);
 	});
-	
+
 }
 
 function set_content_album_links_height() {
